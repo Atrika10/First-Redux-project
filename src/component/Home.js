755ddabc4ae7f65eb34be85d35b09d652be19a5b
ATRIKA SHOW <readeratrika08@gmail.com>
2023-05-14 00:16:54 +0530

    use bindActionCreators

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
-import {actionCreators} from '../state/index'
+import {actionCreators} from '../state/index';
+import { bindActionCreators } from 'redux';
 
 export default function Home() {
    const dispatch = useDispatch();
+   const {actions} = bindActionCreators(actionCreators, dispatch);
   return (
     <div>
        <div className="container"> 
       <div>
         <h2>Buy Your Favorite Product</h2>
-        <button className='btn btn-primary mx-3' onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>- </button>
+        <button className='btn btn-primary mx-3' onClick={()=>{actions.withdrawMoney(100)}}>- </button>
         Update Balance
-        <button className='btn btn-primary mx-3' onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+ </button>
+        <button className='btn btn-primary mx-3' onClick={()=>{actions.depositMoney(100)}}>+ </button>
       </div>
       </div>
     </div>
